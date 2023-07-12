9515fa52f5132397f694b29dd63002f57a45c0a5
m12watanabe1a <40206149+m12watanabe1a@users.noreply.github.com>
2023-07-12 15:05:39 +0900

    Fix wrong type

--- su065d4380_interface/src/su065d4380_interface.cpp
+++ su065d4380_interface/src/su065d4380_interface.cpp
@@ -152,7 +152,7 @@ void SU065D4380Interface::solveError() noexcept
 
 double SU065D4380Interface::getRightVelocity() noexcept
 {
-  constexpr bool COEFFICIENT = RPM2RPS / RIGHT_MECHANICAL_REDUCTION;
+  constexpr double COEFFICIENT = RPM2RPS / RIGHT_MECHANICAL_REDUCTION;
   if (!this->rx_right_vel_packet_->isOK()) {
     return std::numeric_limits<double>::quiet_NaN();
   }
@@ -162,7 +162,7 @@ double SU065D4380Interface::getRightVelocity() noexcept
 
 double SU065D4380Interface::getLeftVelocity() noexcept
 {
-  constexpr bool COEFFICIENT = RPM2RPS / LEFT_MECHANICAL_REDUCTION;
+  constexpr double COEFFICIENT = RPM2RPS / LEFT_MECHANICAL_REDUCTION;
   if (!this->rx_left_vel_packet_->isOK()) {
     return std::numeric_limits<double>::quiet_NaN();
   }
@@ -172,7 +172,7 @@ double SU065D4380Interface::getLeftVelocity() noexcept
 
 double SU065D4380Interface::getRightRadian() noexcept
 {
-  constexpr bool COEFFICIENT = -1.0 * ENC2RAD / RIGHT_MECHANICAL_REDUCTION;
+  constexpr double COEFFICIENT = -1.0 * ENC2RAD / RIGHT_MECHANICAL_REDUCTION;
   if (!this->rx_enc_packet_->isOK()) {
     return std::numeric_limits<double>::quiet_NaN();
   }
@@ -182,7 +182,7 @@ double SU065D4380Interface::getRightRadian() noexcept
 
 double SU065D4380Interface::getLeftRadian() noexcept
 {
-  constexpr bool COEFFICIENT = ENC2RAD / LEFT_MECHANICAL_REDUCTION;
+  constexpr double COEFFICIENT = ENC2RAD / LEFT_MECHANICAL_REDUCTION;
   if (!this->rx_enc_packet_->isOK()) {
     return std::numeric_limits<double>::quiet_NaN();
   }
